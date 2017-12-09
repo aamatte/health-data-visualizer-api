383a9a999356c5f6ec8496dc603f6bd276905473
Andres Matte <matte.andres@gmail.com>
2017-12-09 00:22:56 -0300

    Dynamic routes and available data

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import byline from 'byline';
 import CountiesDataStream from './streams/counties-data-stream';
 import CountiesSearchStream from './streams/counties-search-stream';
 
+const DATA_FILES_DIRECTORY = path.join(__dirname, '../public/files/');
+
 const processQueries = (req, res, next) => {
   const options = {};
   const { state, county, page, perPage } = req.query;
@@ -29,42 +31,34 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/counties', processQueries, (req, res) => {
-  const stream = byline(fs.createReadStream(path.join(__dirname, '../public/files/diabetes-incidence.csv')));
+  const stream = byline(fs.createReadStream(path.resolve(__dirname, '../public/files/diabetes-incidence.csv')));
   stream.pipe(new CountiesSearchStream(req.options)).pipe(concat(data => res.json(data)));
 });
 
-
-routes.get('/diabetes-prevalence', processQueries, (req, res) => {
-  const stream = byline(fs.createReadStream(path.join(__dirname, '..../public/files/diabetes-prevalence.csv')));
-  stream.pipe(new CountiesDataStream(req.options)).pipe(concat((data) => {
-    const responseData = { years: data[0].years, labels: data[1].labels, data: data.slice(2) };
-    res.json(responseData);
-  }));
+routes.get('/available-data', (req, res) => {
+  fs.readdir(DATA_FILES_DIRECTORY, (err, files) => {
+    const availableData = files.map((file, index) => {
+      const route = file.split('.')[0];
+      const name = file.split('.')[0].replace('-', ' ');
+      return ({
+        key: index,
+        path: route,
+        name: `${name.charAt(0).toUpperCase()}${name.slice(1)}`,
+      });
+    });
+    res.json(availableData);
+  });
 });
 
-routes.get('/diabetes-incidence', processQueries, (req, res) => {
-  const stream = byline(fs.createReadStream(path.join(__dirname, '../public/files/diabetes-incidence.csv')));
-  stream.pipe(new CountiesDataStream(req.options)).pipe(concat((data) => {
-    const responseData = { years: data[0].years, labels: data[1].labels, data: data.slice(2) };
-    res.json(responseData);
-  }));
+fs.readdirSync(DATA_FILES_DIRECTORY).forEach((file) => {
+  const [route] = file.split('.');
+  routes.get(`/${route}`, processQueries, (req, res) => {
+    const stream = byline(fs.createReadStream(path.resolve(__dirname, DATA_FILES_DIRECTORY, file)));
+    stream.pipe(new CountiesDataStream(req.options)).pipe(concat((data) => {
+      const responseData = { years: data[0].years, labels: data[1].labels, data: data.slice(2) };
+      res.json(responseData);
+    }));
+  });
 });
-
-routes.get('/obesity-prevalence', processQueries, (req, res) => {
-  const stream = byline(fs.createReadStream(path.join(__dirname, '../public/files/obesity-prevalence.csv')));
-  stream.pipe(new CountiesDataStream(req.options)).pipe(concat((data) => {
-    const responseData = { years: data[0].years, labels: data[1].labels, data: data.slice(2) };
-    res.json(responseData);
-  }));
-});
-
-routes.get('/physical-inactivity', processQueries, (req, res) => {
-  const stream = byline(fs.createReadStream(path.join(__dirname, '../public/files/obesity-prevalence.csv')));
-  stream.pipe(new CountiesDataStream(req.options)).pipe(concat((data) => {
-    const responseData = { years: data[0].years, labels: data[1].labels, data: data.slice(2) };
-    res.json(responseData);
-  }));
-});
-
 
 export default routes;
